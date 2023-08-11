921843800c1d898a1617169e00b867be609fa0ef
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2023-08-11 09:16:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
 ADMINS.append(1905927769)
 #  Optionnal variables
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001916048501")) 
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @") # For Force Subscription
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "STATUSLOVER30") # For Force Subscription
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "false") # true if forward should be avoided
 WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", 'https://snapurl.me/img/member-area-logo.png') # image when someone hit /start # image when someone hit /start
 LINK_BYPASS = "False"
