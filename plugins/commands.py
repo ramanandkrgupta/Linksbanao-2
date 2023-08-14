3cdef77c4c2282fee8a858f9a2377e5a9b46aa14
ramanandkrgupta <115969817+ramanandkrgupta@users.noreply.github.com>
2023-08-14 19:21:48 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -167,7 +167,12 @@ async def me_handler(bot, m:Message):
 
     user_id = m.from_user.id
     user = await get_user(user_id)
+
+    user_name =m.from_user.username
+    user = await get_user(username)
+    
     res = USER_ABOUT_MESSAGE.format(
+                user_name=user["user_name"],
                 user_id=user["user_id"],
                 base_site=user["base_site"],
                 method=user["method"], 
